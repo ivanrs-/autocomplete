c7136eeb68db14759238a3a61fda76ce8b1dcb02
Ivan RS <ivan.ramos@autozone.com>
2020-05-08 18:19:46 -0600

    use IIEF fn, added controls to use with json

--- autosuggest.js
+++ autosuggest.js
@@ -1,19 +1,20 @@
+/** 
+ * Autosuggest Notes V1.0.1
+ * Author: Ivan Ramos <rs.jivan@gmail.com>
+ * Updated: MAY 08, 2020
+ * Description: Provides autocomplete and suggestions capabilities
+ *              for notes field so users can improve their time by
+ *              using predefined sentences or words.
+ * Changes:
+ * IR MAY 08, 2020: Initial testings, using IIFE 
+ */
+AutoSuggest = (() => {
+    const submit = document.getElementById('submit')
+    let content = document.getElementById('notes')
+    let noteAutoSuggest = null;
 
-let notes = document.getElementById('notes')
-let submit = document.getElementById('submit')
-// Submit listener
-document.getElementById('submit').addEventListener('click', (e) => {
-    let data = new Notes(notes.value)
-    data.save()
-});
-
-class AutoSuggest{
-    constructor(input){
-        this.input = input
-    }
-    
-    initControl(){
-        this.noteAutoSuggest = new Awesomplete(this.input, {
+    initControl = () => {
+        noteAutoSuggest = new Awesomplete(content, {
             //list: ["hello", "my", "name", "is", "ari"],
             filter: function (text, input) {
                 return Awesomplete.FILTER_CONTAINS(text, input.match(/[^ ]*$/)[0]);
@@ -28,39 +29,60 @@ class AutoSuggest{
                 this.input.value = before + text + " ";
             }
         })
-    }
+    };
 
-    addListener(){
-        this.input.addEventListener('keyup', (e) => {
+    /**
+     * Add listeners to DOM elements
+     * content -> textarea for notes input
+     * submit  -> submit button
+     */
+    addListeners = () => {
+        // Text area listener
+        content.addEventListener('keyup', (e) => {
             let code = (e.keyCode || e.which);
             if (code === 37 || code === 38 || code === 39 || code === 40 || code === 27 || code === 13)
                 return;
-            
-            const murl = 'https://jsonplaceholder.typicode.com/comments';
-                fetch(murl).then(r =>
-                    r.json()).then(d => {
-                    this.noteAutoSuggest.list = d.map(r => r.name);
-            });
+            // Set control source for data sentences
+            setSource()
+        });
+
+        // Submit listener
+        submit.addEventListener('click', (e) => {
+            Notes.save(content.value)
+        });      
+    };
+
+    /**
+     * 
+     */
+    setSource = () => {
+        //noteAutoSuggest.list= ["hello", "my", "name", "is", "ari"];
+        const murl = 'https://type.fit/api/quotes';
+        fetch(murl).then(r =>
+            r.json()).then(d => {
+            noteAutoSuggest.list = d.map(r => r.text);
         });
     }
-}
 
-class Notes{
-    constructor(notes){
-        this.notes = notes
+    // Expose needed methods
+    return{
+        initControl,
+        addListeners
+    }
+    
+})();
+
+
+/**
+ * Notes operations
+ */
+const Notes =(() => {
+    save = (note) =>  {
+        alert('Proceed to save?\n' + note)
+        document.getElementById('final').value(note)
     }
 
-    save(){
-        alert('Proceed to save?\n'+this.notes)
+    return{
+        save
     }
-}
-
-(() => {
-    document.onreadystatechange =  () => {
-        if (document.readyState === 'interactive') {
-            let note = new AutoSuggest(notes)
-            note.initControl();
-            note.addListener()
-        }
-    }
-})()
+})();
